52e1de8a0a52486c7815e59eec5e649430c4c6f7
Garrick Sir <cwho@ctihe.edu.hk>
2026-06-20 12:33:15 +0800

    Updated.

--- src/run.ipynb.bat
+++ src/run.ipynb.bat
@@ -1,3 +1,3 @@
 # py -3 -m venv .venv
 
-docker run -p 8888:8888 -v "$(pwd)":/home/jovyan/work quay.io/jupyter/base-notebook
+docker run -p 8888:8888 -v .:/home/jovyan/work quay.io/jupyter/base-notebook
